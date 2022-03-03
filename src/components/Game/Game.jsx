f887f8efe1f656db5b0e839af926c5c095ee4334
Hellen Ward <hellenward@Hellens-MacBook-Pro.local>
2022-03-03 07:16:57 +0000

    Began looking at storing guess for each input

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Grid} from '../Grid/Grid';
 import {Input} from '../Input/Input';
 import './Game.css'
 
 export const Game = () => {
+
+    const [guess, setGuess] = useState()
+
+    const handleOnKeyUp = (value, key) => {
+        console.log(value, key)
+        setGuess({key: key, guess: value})
+        console.log('guess', guess)
+    }
+
+    const guessedLetter = (key) => guess.filter(guess => guess.key === key)
+
+
     return (
         <>
             <Grid />
             <p className="Guess-question">What's your guess?</p>
             <div className="guess-container">
-                <Input />
-                <Input />
-                <Input />
-                <Input />
-                <Input />
+                <Input key="1" onKeyUp={(e, key) => handleOnKeyUp(e.target.value, key)} value={(key) => guessedLetter(key)}/>
+                <Input key="2" />
+                <Input key="3" />
+                <Input key="4" />
+                <Input key="5" />
             </div>
         </>
     )
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Input.css';
 
-export const Input = () => {
+export const Input = (props) => {
+
+    let guessedLetter = props.guessedLetter
+    
+    const handleChange = (e) => {
+        guessedLetter = props.guessedLetter
+        console.log(guessedLetter)
+    }
+
     return (
         <form>
-            <input className="Input"></input>
+            <input className="Input" value={props.guessedLetter} onChange={handleChange}></input>
         </form>
     )
 }
